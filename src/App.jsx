ef7c361f877fe38abbc9838d9080a89d92624cc5
Marcus Kwan <kwan.marcus@gmail.com>
2019-07-09 05:31:56 +1000

    changed files to jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 //TODO: STEP 1 - Import the useState hook.
 import React from "react";
 import "./App.css";
-import BottomRow from "./BottomRow";
+import BottomRow from "./BottomRow.jsx";
 
 function App() {
   //TODO: STEP 2 - Establish your applictaion's state with some useState hooks.  You'll need one for the home score and another for the away score.
