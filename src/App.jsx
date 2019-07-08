d5f966c0af1bb50634a65b188062ce9bf8ad7e45
Marcus Kwan <kwan.marcus@gmail.com>
2019-07-09 06:59:19 +1000

    added own recycler function

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,9 @@ function App() {
   //TODO: STEP 2 - Establish your applictaion's state with some useState hooks.  You'll need one for the home score and another for the away score.
   const [homeScore, changeHomeScore] = useState(0);
   const [awayScore, changeAwayScore] = useState(0);
+  // recycler function that lessens the load when you click a button, so you can call it with whatever values you'd like
+  const changeScore = (teamScore, changeTeamScore, incrementAmount) => () =>
+    changeTeamScore(teamScore + incrementAmount);
   return (
     <div className="container">
       <section className="scoreboard">
@@ -30,32 +33,41 @@ function App() {
         <div className="homeButtons">
           {/* TODO STEP 4 - Now we need to attach our state setter functions to click listeners. */}
           <button
-            onClick={() => {
-              changeHomeScore(homeScore + 6);
-            }}
+            // original
+            // onClick={() => {
+            //   changeHomeScore(homeScore + 6);
+            // }}
+            onClick={changeScore(homeScore, changeHomeScore, 6)}
             className="homeButtons__touchdown">
             Home Touchdown
           </button>
           <button
-            onClick={() => {
-              changeHomeScore(homeScore + 3);
-            }}
+            // original
+            // onClick={() => {
+            //   changeHomeScore(homeScore + 3);
+            // }}
+            complicated
+            onClick={changeScore(homeScore, changeHomeScore, 3)}
             className="homeButtons__fieldGoal">
             Home Field Goal
           </button>
         </div>
         <div className="awayButtons">
           <button
-            onClick={() => {
-              changeAwayScore(awayScore + 6);
-            }}
+            // original
+            // onClick={() => {
+            //   changeAwayScore(awayScore + 6);
+            // }}
+            onClick={changeScore(awayScore, changeAwayScore, 7)}
             className="awayButtons__touchdown">
             Away Touchdown
           </button>
           <button
-            onClick={() => {
-              changeAwayScore(awayScore + 3);
-            }}
+            // original
+            // onClick={() => {
+            //   changeAwayScore(awayScore + 3);
+            // }}
+            onClick={changeScore(awayScore,changeAwayScore,3)}
             className="awayButtons__fieldGoal">
             Away Field Goal
           </button>
